aceb9f7108d177e5087d740ab9d30ed8c5c7faab
Дмитрий Карповский <dmkarpovskiy@gmail.com>
2019-02-11 15:25:37 +0200

    initial commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace ConsoleApp2
 {
@@ -10,6 +13,45 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "SELECT * FROM Book2 WHERE Book2.Genre = 3";
+                command.Connection = connection;
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows) // если есть данные
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}", reader.GetName(0), reader.GetName(1), reader.GetName(2), reader.GetName(3));
+
+                    while (reader.Read()) // построчно считываем данные
+                    {
+                        object id = reader.GetValue(0);
+                        object name = reader.GetValue(1);
+                        object author = reader.GetValue(2);
+                        object genre = reader.GetValue(3);
+
+                        Console.WriteLine("{0} \t{1} \t{2} \t{3}", id, name, author, genre);
+                    }
+                }
+
+                reader.Close();
+
+                Console.Read();
+  
+    }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+                Console.WriteLine("Connection was closed");
+            }
+            Console.ReadKey();
         }
     }
 }
